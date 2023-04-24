df70261843c80be73ffefaaf92ac07dd2f7b812b
AntonS <surkov.ved@gmail.com>
2023-04-24 20:46:38 +0300

    added some options to Animals class

--- Models/Animals_ABS.py
+++ Models/Animals_ABS.py
@@ -1,13 +1,21 @@
 from abc import ABC, abstractmethod
 
 
-
 # Абстрактный класс 'Животные'
 class Animals(ABC):
     def __init__(self, name, birthday, command):
         self.name = name
         self.birthday = birthday
         self.command = command
+
+    def get_name(self) -> str:
+        return self.name
+
+    def get_birth_date(self) -> str:
+        return self.birthday
+
+    def get_command(self) -> str:
+        return self.command
 
     @abstractmethod
     def print_animal(self):
